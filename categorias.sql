ad09ca5525e7a4b774a15f98cfdf2e5b6ed4f09f
Julieth0224 <sarazul2002@gmail.com>
2020-11-08 15:42:21 -0500

    Data

---
+++ categorias.sql
@@ -0,0 +1,17 @@
+INSERT INTO public."Categorias" ("Id_categoria", "Tipo_inmueble")
+VALUES(1, 'Apartaestudio');
+
+INSERT INTO public."Categorias" ("Id_categoria", "Tipo_inmueble")
+VALUES(2, 'Apartamento');
+
+INSERT INTO public."Categorias" ("Id_categoria", "Tipo_inmueble")
+VALUES(3, 'Casa');
+
+INSERT INTO public."Categorias" ("Id_categoria", "Tipo_inmueble")
+VALUES(4, 'Oficina');
+
+INSERT INTO public."Categorias" ("Id_categoria", "Tipo_inmueble")
+VALUES(5, 'Lote');
+
+INSERT INTO public."Categorias" ("Id_categoria", "Tipo_inmueble")
+VALUES(6, 'Condominio');
---
+++ inmuebles.sql
@@ -0,0 +1,79 @@
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(1, 1, 'Calle 146 #13-22', 'Cedritos', 56.44, 1, 2, 4, 356964000, 0);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(2, 6, 'Calle 175 #80-15 Casa 13', 'San José de Bavaria', 850, 6, 6, 5, 2950000000, 8);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(3, 3, 'Calle 101 & Cra 71c', 'Potosi Pontevedra', 170, 3, 3, 5, 650000000, 28);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(4, 2, 'Calle 19 & Cra 68c T.3 Apto 205', 'Montevideo', 75, 3, 2, 4, 270000000, 0);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(5, 2, 'Tranversal 2 #67-22 T.7 Apto 408', 'Chapinero', 126, 2, 3, 4, 690000000, 10);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(6, 3, 'Transversal 23 #93-23', 'La Castellana', 380, 5, 4, 4, 1500000000, 25);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(7, 3, 'Cra 50 #100-61', 'Pasadena', 90, 3, 2, 5, 500000000, 20);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(8, 5, 'Calle 137 #50-64', 'San José del Prado', 40, 0, 0, 3, 1100000000, 40);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(9, 1, 'Calle 96 #9-63', 'Chico Norte II', 72, 1, 2, 6, 722000000, 5);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(10, 4, 'Calle 85 #20-05', 'Country', 15.14, 0, 2, 5, 130000000, 5);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(11, 4, 'Cra 19c #89-78', 'Chico Norte III', 45, 0, 1, 5, 360000000, 15);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(12, 6, 'Calle 127a #88d-07 Casa 5', 'Los Naranjos', 230, 3, 5, 6, 1100000000, 16);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(13, 3, 'Cra 100a #72-15', 'Álamos Norte', 207, 4, 4, 3, 550000000, 17);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(14, 2, 'Calle 6c #86-11 T.7 Apto 106', 'Castilla', 61, 3, 2, 3, 215000000, 9);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(15, 2, 'Calle 119 #14b-10 T.5 Apto 302', 'Santa Barbara', 83, 2, 2, 6, 470000000, 11);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(16, 6, 'Calle 81 #73b-10 Casa 8', 'Minuto de Dios', 126, 5, 4, 4, 385000000, 30);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(17, 6, 'Calle 241 #55-07 Casa 23', 'Casa Blanca', 450, 4, 5, 6, 2600000000, 12);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(18, 1, 'Cra 58b #14a-30', 'Chapinero Central', 44, 1, 2, 3, 350000000, 4);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(19, 3, 'Cra 56a #125-10', 'Niza Córdoba', 222, 5, 4, 4, 720000000, 40);
+
+INSERT INTO public."Inmuebles" ("Id_inmueble", "Id_categoria", "Direccion_inmueble", "Sector", "Area",
+                       "Numero_habitaciones", "Numero_baños", "Estrato", "Precio", "Antiguedad")
+VALUES(20, 1, 'Calle 24 #5-20', 'Las Nieves', 42, 1, 1, 2, 250000000, 2);
