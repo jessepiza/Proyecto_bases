7ee8b9296f8384324e51c5294cb4df0bc7a7f0fb
Paula <paulalopezromero7@gmail.com>
2020-11-08 23:29:47 -0500

    Extras de los inmuebles

---
+++ extras.sql
@@ -0,0 +1,323 @@
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(1, 'Piscina', 1, 1);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3,'Parqueadero', 1, 1);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4,'Balcon', 1, 1);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(5,'Parque', 1, 1);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(10, 'Gimnasio', 1, 1);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1, 1);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 1);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 4, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 6, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 6, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(5, 'Parque', 1, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(6, 'Cancha futbol', 1, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(7, 'Cancha baloncesto', 1, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 2, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1, 2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1,2);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 2, 2);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3,'Parqueadero', 2, 3);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 1, 3);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(5, 'Parque', 1, 3);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1,3);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 3);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio',1,3);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(1, 'Piscina',1,4);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 1,4);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(6, 'Cancha futbol',1,4);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1,4);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(10, 'Gimnasio',1,4);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas',1,4);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 1,5);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero',2,5);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 1, 5);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(8, 'Golf', 1, 5);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1, 5);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(10, 'Gimnasio', 1, 5);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 5);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 1, 6);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 2, 6);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 1, 6);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1, 6);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 1, 6);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 1, 7);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas',1, 7);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 1, 7);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 1, 9);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 1, 9);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(10, 'Gimnasio', 1, 9);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 5, 10);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(11, 'Tienda', 1, 10);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 10, 11);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(11, 'Tienda', 1, 11);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(1, 'Piscina',1, 12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 2, 12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero',4,12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 2, 12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(8, 'Golf', 1, 12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1, 12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1, 12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 12);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 1, 12);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 1, 13);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 1, 13);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1, 13);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 13);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 1, 13);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 1, 14);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1, 14);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza',1, 15);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero',1, 15);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(7, 'Cancha baloncesto', 1, 15);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(11, 'Tienda', 1, 15);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 2, 16);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 3, 16);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1, 16);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1, 16);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 16);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 1, 16);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 2, 17);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 5, 17);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1, 17);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 2, 17);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 17);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 2, 17);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 4, 17);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 1, 18);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(10, 'Gimnasio', 1, 18);
+
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(2, 'Terraza', 2, 19);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 3, 19);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1, 19);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(12, 'Chimenea', 1, 19);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas', 1, 19);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(14, 'Patio', 2, 17);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 4, 19);
+
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(3, 'Parqueadero', 2, 20);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(4, 'Balcon', 1, 20);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(9, 'Salon comunal', 1, 20);
+
+INSERT INTO PUBLIC. "Extras"("Id_extra","Nombre_extra", "Cantidad", "Id_inmueble")
+VALUES(13, 'Cuarto de ropas',1, 20);
