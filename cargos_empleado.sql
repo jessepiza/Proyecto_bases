24d6a7d773fa51e356cebf850ae26ef3e9d4a3f0
valelopezch <anava.lopez@urosario.edu.co>
2020-11-08 20:30:21 -0500

    Modelo Entidad-Relacion

--- cargos_empleado.sql
+++ cargos_empleado.sql
@@ -5,4 +5,5 @@ VALUES('Domiciliario',1000000,48);
 INSERT INTO PUBLIC. "Cargos_empleado"("Nom_cargo", "Salario", "Horas_semanales")
 VALUES('Administrador',2000000,48);
 
-INSERT INTO PUBLIC. "Cargos_empleado"("Nom_cargo", "Salario", "Horas_semanales")VALUES('Gerente',1500000,48);
+INSERT INTO PUBLIC. "Cargos_empleado"("Nom_cargo", "Salario", "Horas_semanales")
+VALUES('Gerente',1500000,48);
